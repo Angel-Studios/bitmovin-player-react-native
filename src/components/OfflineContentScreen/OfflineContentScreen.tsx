f6b78305d4ed89eb2c16113562d33dda1155b3ae
jfspencerAngel <james.spencer@angel.com>
2022-11-30 11:15:57 -0700

    chore: wip

--- src/components/OfflineContentScreen/OfflineContentScreen.tsx
+++ src/components/OfflineContentScreen/OfflineContentScreen.tsx
@@ -1,5 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { FlatList, Pressable, SafeAreaView, Text, View } from 'react-native';
 
-export const OfflineContentScreen = () => {
-  return <></>;
+export const OfflineContentScreen = ({
+  onContentSelected,
+}: {
+  onContentSelected: (guid?: string) => void;
+}) => {
+  const [offlineItems, setOfflineItems] = useState<Array<unknown>>();
+  useEffect(() => {
+    setOfflineItems([]);
+  }, [offlineItems]);
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Header</Text>
+      </View>
+      <FlatList data={[]} renderItem={OfflineContentItem(onContentSelected)} />
+    </SafeAreaView>
+  );
 };
+
+const OfflineContentItem =
+  (onContentSelected: (guid?: string) => void) =>
+  ({ item }: { item: { title?: string; guid?: string } }) => {
+    return (
+      <Pressable onPress={() => onContentSelected(item.guid)}>
+        <Text>{item.title}</Text>
+      </Pressable>
+    );
+  };
