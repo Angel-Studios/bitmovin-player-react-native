aa8df05c4083baf586fd0dd7a3cfd7ad58697889
Nick Graves <ngcubed@gmail.com>
2023-08-31 10:16:23 -0600

    test: network config

--- ios/RCTConvert+BitmovinPlayer.swift
+++ ios/RCTConvert+BitmovinPlayer.swift
@@ -52,7 +52,7 @@ extension RCTConvert {
                 }
             }
             var delegate = RequestDelegate()
-            // networkConfig.preprocessHttpRequestDelegate = delegate
+            networkConfig.preprocessHttpRequestDelegate = delegate
         }
         return networkConfig
     }
